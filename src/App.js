2abf2746c0545aaaeb1f5361801f0751cf62576d
unknown <mohdarif013579@gmail.com>
2022-06-21 11:20:18 +0530

    redux saga architecture

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+ import './App.css';
 
 function App() {
+// why react is fast --> it is only updated specific dom
+// how does react js works --> compare actual dom and virtual dom
+// diffi algorithims
+
+//what is redux?
+//1. Libraray for state management.
+//2. Libraray for global data management.
+//3. Redux - A predictable state container
+//4. can share data in ny component to any component.
+
+// Redux Archieture
+
+// action -->(saga operations)---> reducers--> store
+//[component] -- fetch to any components
+
+// Redux wrapper all application.
+
+
+// what is Saga ?
+// 1. handle async data in redux like apis data
+//2.We call it middleware
+//3. Middleware between reacting anf redux
+//4. Here we can write async operations like time taking logic and api calls
+
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
