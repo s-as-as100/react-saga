ed7788414d3f98a9d4ab4e23ce654c6704d22cb2
unknown <mohdarif013579@gmail.com>
2022-06-21 11:35:40 +0530

    Create reduc wrapper provider

--- src/App.js
+++ src/App.js
@@ -1,34 +1,7 @@
  import './App.css';
 
 function App() {
-// why react is fast --> it is only updated specific dom
-// how does react js works --> compare actual dom and virtual dom
-// diffi algorithims
-
-//what is redux?
-//1. Libraray for state management.
-//2. Libraray for global data management.
-//3. Redux - A predictable state container
-//4. can share data in ny component to any component.
-
-// Redux Archieture
-
-// action -->(saga operations)---> reducers--> store
-//[component] -- fetch to any components
-
-// Redux wrapper all application.
-
-
-// what is Saga ?
-// 1. handle async data in redux like apis data
-//2.We call it middleware
-//3. Middleware between reacting anf redux
-//4. Here we can write async operations like time taking logic and api calls
-
-
-
-
-
+ 
   return (
     <div className="App">
       
